71b345e40b19c385654cd63c89631dde1d3151e8
Kasra Karami <Kasra_K2K@yahoo.com>
2023-05-28 15:14:35 +0330

    fix sqs and add functions

--- sqs/index.js
+++ sqs/index.js
@@ -1,26 +1,69 @@
-const {
-  SQSClient,
-  AddPermissionCommand,
-  ListQueuesCommand,
-  CreateQueueCommand,
-} = require("@aws-sdk/client-sqs")
+const { SQS } = require("@aws-sdk/client-sqs")
 
-const sqsClient = new SQSClient({
+const sqs = new SQS({
   region: "us-east-1",
   endpoint: "http://localhost:4566",
 })
-const queueName = "my-queue-1"
+const QueueName = "my-queue-1"
+const QueueUrl = "http://localhost:4566/000000000000/my-queue-1"
 
-async function createSQSQueue() {
-  const command = new CreateQueueCommand({ QueueName: queueName })
-  const response = await sqsClient.send(command)
-  console.log(response)
+async function createQueue() {
+  sqs
+    .createQueue({ QueueName })
+    .then((result) => result.QueueUrl)
+    .catch((err) => console.log(err.message))
 }
-createSQSQueue()
+// createQueue()
 
-async function listSQSQueues() {
-  const command = new ListQueuesCommand({ QueueNamePrefix: "" })
-  const response = await sqsClient.send(command)
-  console.log(response.QueueUrls)
+async function listQueues() {
+  sqs
+    .listQueues({ QueueNamePrefix: "" })
+    .then((result) => result.QueueUrls)
+    .catch((err) => console.log(err.message))
 }
-listSQSQueues()
+// listQueues()
+
+async function deleteQueue() {
+  sqs
+    .deleteQueue({ QueueUrl })
+    .then(() => console.log(`Queue delete successfuly`))
+    .catch((err) => console.log(err.message))
+}
+// deleteQueue()
+
+async function purgeQueue() {
+  sqs
+    .purgeQueue({ QueueUrl })
+    .then(() => console.log(`Queue purge successfuly`))
+    .catch((err) => console.log(err.message))
+}
+// purgeQueue()
+
+async function sendMessage() {
+  sqs
+    .sendMessage({ QueueUrl, MessageBody: "my message body 1" })
+    .then((result) => console.log(result.MessageId))
+    .catch((err) => console.log(err.message))
+}
+// sendMessage()
+
+async function receiveMessage() {
+  sqs
+    .receiveMessage({ QueueUrl, MaxNumberOfMessages: 10, VisibilityTimeout: 0 })
+    .then((result) => {
+      console.log(result.Messages)
+      for (const message of result.Messages ?? []) {
+        // Do something with message then delete message
+        deleteMessage(message.ReceiptHandle)
+      }
+    })
+    .catch((err) => console.log(err.message))
+}
+// receiveMessage()
+
+async function deleteMessage(ReceiptHandle) {
+  sqs
+    .deleteMessage({ QueueUrl, ReceiptHandle })
+    .then(() => console.log(`Message delete successfuly.`))
+    .catch((err) => console.log(err.message))
+}
